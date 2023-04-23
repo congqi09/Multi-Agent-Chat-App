e239557560e636dd2248072b8a5dddc2a117125c
adelaide2021 <xueyangli2015@gmail.com>
2023-04-22 20:46:12 -0400

    add mongo database and transfer to maven project

---
+++ db/src/DB.java
@@ -0,0 +1,26 @@
+import static com.mongodb.client.model.Filters.eq;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class DB {
+    public static void main( String[] args ) {
+
+        String uri = "mongodb+srv://finalproject:U3NYaVURLFT7S9xV@cluster0.nlf6m5v.mongodb.net/?retryWrites=true&w=majority";
+
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
+            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
+    }
+}
