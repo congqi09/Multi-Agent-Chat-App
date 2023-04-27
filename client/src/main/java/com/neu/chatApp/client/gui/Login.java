d84a4477e3c5015917574063d2f3e6e1fb512928
wenjie qi <qi.wenj@northeastern.edu>
2023-04-27 02:33:53 -0400

    add gui

---
+++ client/src/main/java/com/neu/chatApp/client/gui/Login.java
@@ -0,0 +1,14 @@
+package com.neu.chatApp.client.gui;
+
+import javax.swing.*;
+
+public class Login {
+  JPanel panelMain;
+  JTextField usernameField;
+  private JLabel usernameTitle;
+  JTextField passwordField;
+  private JLabel passwordTitle;
+  JButton loginButton;
+  JButton signup;
+
+}
